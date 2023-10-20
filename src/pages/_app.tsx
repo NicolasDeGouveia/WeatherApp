125ae870492b7ca8d3385cca2fd050efdaff6df7
NicolasDeGouveia <nicolas.degouveia@gmail.com>
2023-10-20 09:49:32 +0200

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ReactQueryDevtools />
+      </Hydrate>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
